4ca60b77a2703f615a690991c447218246c7b695
mitchelle <mitchellenjoki7@gmail.com>
2022-06-10 00:39:52 +0300

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+const batteryBatches = [4, 5, 3, 4, 4, 6, 5]; 
+
+
+const totalBatteries = batteryBatches.reduce(function(allBatteries, batches){
+    return batches + allBatteries
+    }
+  )
+  
 
 // Code your solution here
